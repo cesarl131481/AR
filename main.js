702dac75a75976bed58dae1fdb7b5cad62af83db
cesarl131481 <115443038+cesarl131481@users.noreply.github.com>
2025-10-08 20:40:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const sceneEl = document.querySelector("a-scene");
   const loading = document.getElementById("loading");
 
-  sceneEl.addEventListener("loaded", () => {
-    loading.style.display = "none";
-  });
+  const mindarSystem = sceneEl.systems["mindar-image-system"];
+  const target0 = document.querySelector("#target0");
+  const target1 = document.querySelector("#target1");
+  //Ocultar objetos
+  target0.object3D.visible = false;
+  target1.object3D.visible = false;
+  //Deteccion de marcador
+  sceneEl.addEventListener("targetFound", (event)=>{
+    const index = event.detail.index;
+    if(index === 0) target0.object3D.visible = true;
+    if(index === 1) target1.object3D.visible = true;
+    });
+  //Perdida del marcador
+  sceneEl.addEventListener("targetLost", (event)=>{
+    const index = event.detail.index;
+    if(index === 0) target0.object3D.visible = false;
+    if(index === 1) target1.object3D.visible = false;
+    });
 });
